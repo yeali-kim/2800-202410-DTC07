fb92ea701f259a16cf2b56b62629a661049be654
Shawn Rim <trim2@my.bcit.ca>
2024-05-13 12:52:52 -0700

    Initialize ejs and create endpoints

--- scripts/server.js
+++ scripts/server.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
 
-
 const mongoose = require('mongoose')
 
 main().catch(err => console.error('MongoDB connection error:', err)); // Log MongoDB connection errors
 
 async function main() {
-  try {
-    await mongoose.connect('mongodb+srv://Daniel:2foADJtqgvjQ8CMA@cluster0.n7n7slw.mongodb.net/?retryWrites=true&w=majority');
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    throw error; // Rethrow the error to stop the application startup
-  }
+  // try {
+  //   await mongoose.connect('mongodb+srv://Daniel:2foADJtqgvjQ8CMA@cluster0.n7n7slw.mongodb.net/?retryWrites=true&w=majority');
+  //   console.log('Connected to MongoDB');
+  // } catch (error) {
+  //   console.error('MongoDB connection error:', error);
+  //   throw error; // Rethrow the error to stop the application startup
+  // }
 }
 
 const criminalSchema = new mongoose.Schema({
@@ -27,6 +26,8 @@ const criminalSchema = new mongoose.Schema({
 // Ensure that the model name matches the actual collection name
 const CriminalProfile = mongoose.model('criminalProfile', criminalSchema);
 
+app.set("view engine", "ejs");
+
 app.get('/crime', async (req, res) => {
   try {
     const allCriminals = await CriminalProfile.find({});
@@ -36,3 +37,43 @@ app.get('/crime', async (req, res) => {
     res.status(500).json({ error: 'Internal server error' });
   }
 });
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get('/login', (req, res) => {
+  res.render("login");
+})
+
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+app.get("/map", (req, res) => {
+    res.render("map");
+});
+
+app.get("/list", (req, res) => {
+    res.render("list");
+});
+
+app.get("/protection", (req, res) => {
+    res.render("protection");
+});
+
+app.get("/robots", (req, res) => {
+    res.render("login");
+});
+
+app.get("/drones", (req, res) => {
+    res.render("login");
+});
+
+app.get("/cybersecurity", (req, res) => {
+    res.render("login");
+});
+
+app.listen(3000, () => {
+  console.log("Listening to Port 3000.")
+})
