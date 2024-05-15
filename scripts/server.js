d467c466dd0ff88e75aa52654412a2d145614cf5
Shawn Rim <trim2@my.bcit.ca>
2024-05-15 13:59:08 -0700

    Initialize profile page

--- scripts/server.js
+++ scripts/server.js
@@ -16,35 +16,37 @@ const PORT = process.env.PORT || 3000;
 const saltRounds = Number(process.env.SALT_ROUNDS);
 const expireTime = 1000 * 60 * 60;
 
-main().catch(err => console.error('MongoDB connection error:', err)); // Log MongoDB connection errors
+main().catch((err) => console.error("MongoDB connection error:", err)); // Log MongoDB connection errors
 
 async function main() {
-  try {
-    await mongoose.connect(`mongodb+srv://${mongo_user}:${mongo_password}@${mongo_host}/${mongo_db}`);
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    throw error; // Rethrow the error to stop the application startup
-  }
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${mongo_user}:${mongo_password}@${mongo_host}/${mongo_db}`
+        );
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        throw error; // Rethrow the error to stop the application startup
+    }
 
-  app.listen(PORT, () => {
-      console.log("Listening to port "+PORT);
-  });
+    app.listen(PORT, () => {
+        console.log("Listening to port " + PORT);
+    });
 }
 
 const criminalSchema = new mongoose.Schema({
-  name: String,
-  DOB: String,
-  coordinates: Object,
-  date_of_crime: String,
-  sentence: String
+    name: String,
+    DOB: String,
+    coordinates: Object,
+    date_of_crime: String,
+    sentence: String,
 });
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
-})
+    username: String,
+    email: String,
+    password: String,
+});
 
 // Ensure that the model name matches the actual collection name
 const CriminalProfile = mongoose.model("criminalProfile", criminalSchema);
@@ -65,131 +67,131 @@ app.use(
 
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/crimes', async (req, res) => {
-  try {
-    const allCriminals = await CriminalProfile.find();
-    console.log(allCriminals)
-    res.json(allCriminals);
-  } catch (error) {
-    console.error('Error fetching criminal data:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+app.get("/crimes", async (req, res) => {
+    try {
+        const allCriminals = await CriminalProfile.find();
+        console.log(allCriminals);
+        res.json(allCriminals);
+    } catch (error) {
+        console.error("Error fetching criminal data:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 });
 
-app.get('/createCrimes', async (req, res) => {
-  await CriminalProfile.create();
-  res.send('Completed')
-})
+app.get("/createCrimes", async (req, res) => {
+    await CriminalProfile.create();
+    res.send("Completed");
+});
 
-app.get('/users', async(req, res) => {
-  const users = await Users.find();
-  res.json(users)
-})
+app.get("/users", async (req, res) => {
+    const users = await Users.find();
+    res.json(users);
+});
 
 app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.get('/login', (req, res) => {
-  res.render("login");
-})
+app.get("/login", (req, res) => {
+    res.render("login");
+});
 
-app.post('/login', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+app.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
-  const schema = Joi.object({
-      password: Joi.string().max(20).required(),
-      email: Joi.string().email({
-          minDomainSegments: 2,
-          tlds: { allow: ["com", "ca"] },
-      }),
-  });
+    const schema = Joi.object({
+        password: Joi.string().max(20).required(),
+        email: Joi.string().email({
+            minDomainSegments: 2,
+            tlds: { allow: ["com", "ca"] },
+        }),
+    });
 
-  const validationResult = schema.validate({ password, email });
-  if (validationResult.error != null) {
-      console.log(validationResult.error);
-      res.redirect("/login");
-      return;
-  }
+    const validationResult = schema.validate({ password, email });
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.redirect("/login");
+        return;
+    }
 
-  req.session.email = email;
-  req.session.password = password;
+    req.session.email = email;
+    req.session.password = password;
 
-  res.redirect('/validateUser');
-})
+    res.redirect("/validateUser");
+});
 
 app.get("/signup", (req, res) => {
-  res.render("signup");
+    res.render("signup");
 });
 
 app.post("/signup", async (req, res) => {
-  const username = req.body.name;
-  const email = req.body.email;
-  const password = req.body.password;
+    const username = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
 
-  const schema = Joi.object({
-      username: Joi.string().alphanum().max(20).required(),
-      password: Joi.string().max(20).required(),
-      email: Joi.string().email({
-          minDomainSegments: 2,
-          tlds: { allow: ["com", "ca"] },
-      }),
-  });
+    const schema = Joi.object({
+        username: Joi.string().alphanum().max(20).required(),
+        password: Joi.string().max(20).required(),
+        email: Joi.string().email({
+            minDomainSegments: 2,
+            tlds: { allow: ["com", "ca"] },
+        }),
+    });
 
-  const validationResult = schema.validate({ username, password, email });
-  if (validationResult.error != null) {
-      console.log(validationResult.error);
-      res.redirect("/signup");
-      return;
-  }
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const validationResult = schema.validate({ username, password, email });
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.redirect("/signup");
+        return;
+    }
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-  await Users.create({
-    username: username,
-    email: email,
-    password: hashedPassword,
-  })
+    await Users.create({
+        username: username,
+        email: email,
+        password: hashedPassword,
+    });
 
-  req.session.email = email;
-  req.session.password = password;
+    req.session.email = email;
+    req.session.password = password;
 
-  res.redirect('/validateUser');
-})
+    res.redirect("/validateUser");
+});
 
-app.get('/resetPassword', (req, res) => {
-  res.render('resetPassword');
-})
+app.get("/resetPassword", (req, res) => {
+    res.render("resetPassword");
+});
 
-app.post('resetPassword', async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+app.post("resetPassword", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
-  const schema = Joi.object({
-      username: Joi.string().alphanum().max(20).required(),
-      password: Joi.string().max(20).required(),
-      email: Joi.string().email({
-          minDomainSegments: 2,
-          tlds: { allow: ["com", "ca"] },
-      }),
-  });
+    const schema = Joi.object({
+        username: Joi.string().alphanum().max(20).required(),
+        password: Joi.string().max(20).required(),
+        email: Joi.string().email({
+            minDomainSegments: 2,
+            tlds: { allow: ["com", "ca"] },
+        }),
+    });
 
-  const validationResult = schema.validate({ username, password, email });
-  if (validationResult.error != null) {
-      console.log(validationResult.error);
-      res.redirect("/signup");
-      return;
-  }
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const validationResult = schema.validate({ username, password, email });
+    if (validationResult.error != null) {
+        console.log(validationResult.error);
+        res.redirect("/signup");
+        return;
+    }
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-  const user = await Users.findOne({ email: email });
+    const user = await Users.findOne({ email: email });
 
-  if (user) {
-    await Users.updateOne({ email: email }, { password: hashedPassword});
-  }
+    if (user) {
+        await Users.updateOne({ email: email }, { password: hashedPassword });
+    }
 
-  res.redirect('/login');
-})
+    res.redirect("/login");
+});
 
 async function validateUser(req, res, next) {
     if (req.session.email && req.session.password) {
@@ -205,8 +207,8 @@ async function validateUser(req, res, next) {
     res.redirect("/login");
 }
 
-app.use('/validateUser', validateUser, (req, res) => {
-  res.redirect('/map')
+app.use("/validateUser", validateUser, (req, res) => {
+    res.redirect("/map");
 });
 
 app.get("/map", (req, res) => {
@@ -233,9 +235,10 @@ app.get("/cybersecurity", (req, res) => {
     res.render("login");
 });
 
-app.get('/profile', (req, res) => {
-  res.render("profile", { user: req.session.user });
-})
+app.get("/profile", async (req, res) => {
+    const user = await Users.find({ username: "User1" });
+    res.render("profile", { user: user });
+});
 
 app.get("/logout", validateUser, (req, res) => {
     req.session.destroy();
@@ -243,6 +246,6 @@ app.get("/logout", validateUser, (req, res) => {
 });
 
 app.get("/*", (req, res) => {
-  res.status(404);
-  res.send("404: Page Not Found");
-})
+    res.status(404);
+    res.send("404: Page Not Found");
+});
